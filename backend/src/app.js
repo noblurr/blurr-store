ca363bfccb87ac71ae7a07dfaf6df8d4b871e401
noblurr <wrasenganchik@gmail.com>
2026-03-19 13:51:56 +0500

    fix port for railway

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import productsRoutes from "./routes/products.js";
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,6 +18,6 @@ app.get("/", (req, res) => {
   res.send("API работает 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
